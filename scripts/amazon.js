850f0459b09dbf161787d868e391674ad6e004f6
M31 <foadams.dev@gmail.com>
2025-11-10 13:43:14 +0000

    saved data for rendering homepage

---
+++ scripts/amazon.js
@@ -0,0 +1,82 @@
+
+const product = [
+    {
+        id:0,
+        name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
+        image:"images/athletic-cotton-socks-6-pairs.jpg",
+        rating:"images/rating-45.png",
+        reviews: 87,
+        price: 10.90,
+    },
+     {
+        id:1,
+        name:"Intermediate Size Basketball",
+        image:"images/intermediate-composite-basketball.jpg",
+        rating:"images/rating-4.png",
+        reviews: 127,
+        price: 20.95,
+    },
+     {
+        id:2,
+        name:"Adults Plain Cotton T-Shirt - 2 Pack",
+        image:"images/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+        rating:"images/rating-45.png",
+        reviews: 56,
+        price: 7.99,
+    },
+     {
+        id:3,
+        name:"2 Slot Toaster - Black",
+        image:"images/black-2-slot-toaster.jpg",
+        rating:"images/rating-5.png",
+        reviews: 2197,
+        price: 18.99,
+    },
+     {
+        id:4,
+        name:"6 Piece White Dinner Plate Set",
+        image:"images/6-piece-white-dinner-plate-set.jpg",
+        rating:"images/rating-4.png",
+        reviews: 37,
+        price: 20.67,
+    },
+     {
+        id:5,
+        name:"6-Piece Nonstick, Carbon Steel Oven",
+        image:"images/6-piece-non-stick-baking-set.webp",
+        rating:"images/rating-45.png",
+        reviews: 175,
+        price: 34.99,
+    },
+      {
+        id:6,
+        name:"Plain Hooded Fleece Sweatshirt",
+        image:"images/plain-hooded-fleece-sweatshirt-yellow.jpg",
+        rating:"images/rating-45.png",
+        reviews: 317,
+        price: 24.00,
+    },
+      {
+        id:7,
+        name:"Luxury Towel Set - Graphite Gray",
+        image:"images/luxury-tower-set-6-piece.jpg",
+        rating:"images/rating-45.png",
+        reviews: 144,
+        price: 35.99,
+    },
+    {
+        id:8,
+        name:"Liquid Laundry Detergent, 110 Loads",
+        image:"images/liquid-laundry-detergent-plain.jpg",
+        rating:"images/rating-45.png",
+        reviews: 305,
+        price: 28.99,
+    },
+   
+   
+   
+
+
+
+
+]
