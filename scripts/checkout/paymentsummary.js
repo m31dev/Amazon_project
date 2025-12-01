230080868b7b4348cffcc06b561b29cad9670240
M31 <foadams.dev@gmail.com>
2025-12-01 08:27:19 +0000

    update

--- scripts/checkout/paymentsummary.js
+++ scripts/checkout/paymentsummary.js
@@ -1,15 +1,22 @@
 import { cart } from "../../data/cart.js"
+import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { getProduct } from "../../data/product.js"
 import { formatCurrency } from "../../utils/money.js";
 
 export function renderPaymentSummary(){
     let total = 0;
+    let shippingPrice = 0;
     cart.forEach((item)=>{
         const product = getProduct(item.productId)
         total += product.price * item.quantity;
+
+        const deliveryOption = getDeliveryOption(item.deliveryOptionId)
+        shippingPrice += deliveryOption.price
        
     })
-    total = formatCurrency(total)
-    console.log(total)
+   const totalbeforeTax = total + shippingPrice;
+   const tax = totalbeforeTax * 0.1;
+   const totalPrice = totalbeforeTax + tax;
+   console.log(totalPrice)
     
 }
