e43f8ee13b7d2aecd461a79435b6159131de88f0
M31 <foadams.dev@gmail.com>
2025-12-05 11:29:50 +0000

    update

---
+++ data/cart-class.js
@@ -0,0 +1,61 @@
+//Cart class for generating object
+class Cart{
+    cartItems = undefined;
+    localStorageKey = undefined;
+
+    constructor(cartItems, localStorageKey){
+        this.cartItems = cartItems
+        this.localStorageKey = localStorageKey;
+    }
+
+    //Methods
+    loadFromLocalStorage(){
+       this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) ||
+       [
+        {
+            productId:'0',
+            quantity:1,
+            deliveryOptionId:'0'
+        },
+        {
+            productId:'1',
+            quantity:2,
+            deliveryOptionId:'1'
+        }
+       ] 
+    }
+
+    saveToStorage(){
+    localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems))
+    }
+
+    updateDelOption(productId,deliveryOptionId){
+    let matchingItem;
+    this.cartItems.forEach((item)=>{
+        if(productId === item.productId){
+            matchingItem = item;
+        }
+    })
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    this.saveToStorage()
+    }
+
+    removeItem(prodId){
+    const newCart = [];
+    this.cartItems.forEach((cartItem)=>{
+        if(cartItem.productId !== prodId){
+            newCart.push(cartItem)
+        }
+    })
+    this.cartItems = newCart;
+    this.saveToStorage()
+    }
+
+}
+
+const cart = new Cart('cart-oop')
+const business = new Cart('business');
+cart.loadFromLocalStorage()
+business.loadFromLocalStorage()
+
+console.log(cart)
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderItems } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 renderItems()
 renderPaymentSummary();
