4d769ea1229a8dea253ccd45546e8e3f583fbff1
M31 <foadams.dev@gmail.com>
2025-12-05 10:28:05 +0000

    update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderItems } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
+import '../data/cart-oop.js'
 renderItems()
 renderPaymentSummary();
